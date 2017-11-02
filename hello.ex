4cd0d22b9a686d2cf9e619b32f3212a4eca6ac8e
Hironori Akaishi <ah.gm3622@gmail.com>
2017-11-02 21:20:39 +0900

    initial commit

---
+++ hello.ex
@@ -0,0 +1,9 @@
+# コンパイルする場合
+defmodule Hello do
+
+  def world do
+    IO.puts "HelloWorld"
+  end
+end
+
+Hello.world
---
+++ hello.exs
@@ -0,0 +1,9 @@
+# コンパイルしない場合
+defmodule Hello do
+
+  def world do
+    IO.puts "HelloWorld"
+  end
+end
+
+Hello.world
